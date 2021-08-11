85ce26a0c1436149a6e6747d864d24fdf73d928e
Ayman Hassanin <87046847+Ayman-Hassanin@users.noreply.github.com>
2021-08-11 17:22:23 +0200

    Delete Ayman - Refracting Surfaces directory

--- Ayman - Refracting Surfaces/CALCSHOWRESULT2Ay.m
+++
@@ -1,8 +0,0 @@
-function CALCSHOWRESULT2Ay(N1,N2,P)
-    I=-(N2/N1)*P;
-    disp('The Image Chracteristics');
-    disp('1. It Is the SAME SIZE and ORIENTATION as the Object');
-    disp('2. It Is Virtual');
-    disp('3. It Is On THE SAME SIDE Relative to the Object');
-    fprintf('4. It Is (%0.2f cm) from the surface of separation\n',-I);
-end
